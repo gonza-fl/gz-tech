be7daa6d7e050fe1cfb92783a6b891c577caadd3
Gonza[TFKL] <gonzaloflamenci4@gmail.com>
2025-08-26 19:16:52 -0300

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gonza-fl.github.io',
-  base: '/gz-tech/',
   integrations: [tailwind(), react()],
 });
